d8a8eb9734463f8ae85445515c21982cb417687a
cshsf <hakenny63@gmail.com>
2025-03-04 19:30:04 +0800

    create script

---
+++ temp.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         hello world
+// @namespace    http://csff.com/
+// @version      2025-03-04
+// @description  new script
+// @author       ff
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log("hello world");
+})();
